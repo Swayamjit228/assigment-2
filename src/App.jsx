2e1b9117a6139d5da4b858bffb75f0d7c83fadff
Swayamjit228 <24cseaiml228.swayamjitsasmal@giet.edu>
2026-09-04 11:21:18 +0530

    Add React assignments

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Q1 from "./Q1";
+import Q2 from "./Q2";
+import Q3 from "./Q3";
+
+function App() {
+  return (
+    <div>
+      <Q1 />
+      <hr />
+      <Q2 />
+      <hr />
+      <Q3 />
+    </div>
+  );
+}
+
+export default App;
